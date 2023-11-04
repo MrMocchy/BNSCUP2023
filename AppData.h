b461a7e3788f46186335c41fb805c6aa428c8f06
MrMocchy <patronummocchy@tiara.ocn.ne.jp>
2023-11-04 21:59:54 +0900

    救助者に浮き輪

--- AppData.h
+++ AppData.h
@@ -19,6 +19,7 @@ struct GameData {
 		double waveOffset;
 		double drowningTime;
 		bool isSaved = false;
+		bool isDead = false;
 	};
 	Array<Human> people;
 	const size_t peopleNum = 100;
@@ -26,6 +27,7 @@ struct GameData {
 	// 絵文字のデザインによって異なる顔のサイズを揃えるための定数
 	double faceSizeS = 30.0;
 	double faceSizeL = 40.0;
+	double swimRingSize = 30.0;
 	//溺れ
 	size_t maxDrowningNum = 2;
 	double drownTime = 5.0;
@@ -40,7 +42,7 @@ struct GameData {
 		}
 
 		// 人の作成
-		for (auto i : step(faceNum))
+		for (auto i : step(peopleNum))
 		{
 			people.push_back({ RandomVec2(sea), Random(faceNum-1), Random(0.0, 360.0_deg),0.0 });
 			//people.push_back({ Vec2(50*i,300), i, Random(0.0, 360.0_deg)});
--- GameScene.h
+++ GameScene.h
@@ -11,7 +11,7 @@ public:
 	void update() override
 	{
 		auto& data = getData();
-		Transformer2D transform(Mat3x2::Translate(-data.viewX, 0.0));
+		Transformer2D transform(Mat3x2::Translate(-data.viewX, 0.0), TransformCursor::Yes);
 
 		//左右の視点移動
 		data.viewX += data.viewScrollSpeed * Scene::DeltaTime()
@@ -21,23 +21,23 @@ public:
 		for (auto& human : data.people) {
 			if (human.drowningTime > 0) human.drowningTime += Scene::DeltaTime();
 
-			if (human.drowningTime > data.drownTime && not human.isSaved) {
+			if (human.drowningTime > data.drownTime && (not human.isSaved && not human.isDead)) {
 				//ゲームオーバー画面へ遷移
 				Print << U"GameOver";
-				human.isSaved = true;
+				human.isDead = true;
 			}
 		}
 		//溺れている人数が最大人数より少ない場合、ランダムに人を溺れさせる
-		size_t drowningNum = data.people.count_if([](GameData::Human h) {return not h.isSaved && h.drowningTime > 0; });
+		size_t drowningNum = data.people.count_if([](GameData::Human h) {return (not h.isSaved && not h.isDead) && h.drowningTime > 0; });
 		if (drowningNum < data.maxDrowningNum) {
 			data.people[Random(size_t(0),data.people.size()-1)].drowningTime += Scene::DeltaTime();
 		}
 
 		for (auto& human : data.people)
 		{
-			if (human.drowningTime == 0)continue;
+			if (human.drowningTime == 0 || human.isSaved || human.isDead)continue;
 			//溺れている人の救助判定
-			if (MouseL.down() && (human.pos - Cursor::PosF()).length() < 40) {
+			if (MouseL.down() && (human.pos + Vec2(0, 10) * Math::Sin(human.waveOffset + Time::GetMillisec() / 1000.0) - Cursor::PosF()).length() < 40) {
 				Print << U"Saved";
 				human.isSaved = true;
 			}
@@ -47,7 +47,7 @@ public:
 	void draw() const override
 	{
 		auto& data = getData();
-		Transformer2D transform(Mat3x2::Translate(-data.viewX, 0.0));
+		Transformer2D transform(Mat3x2::Translate(-data.viewX, 0.0),TransformCursor::Yes);
 
 		//背景描画
 		data.sky.draw(Palette::Skyblue);
@@ -57,8 +57,29 @@ public:
 		//人描画
 		for (auto& human : data.people)
 		{
+			//波アニメーション
+			Vec2 pos = human.pos + Vec2(0, 10) * Math::Sin(human.waveOffset + Time::GetMillisec() / 1000.0);
+
+			//浮き輪奥
+			if (human.isSaved)
+			{
+				Transformer2D t(Mat3x2::Scale(1, 0.6,pos));
+				for (auto i : step(4)) {
+					Circle{ pos,data.swimRingSize }.drawArc(-90_deg + 45_deg * i, 45_deg,data.swimRingSize / 3, 0, i % 2 == 0 ? Palette::White : Palette::Red);
+				}
+			}
+
 			TextureAsset(U"face{}"_fmt(human.faceIndex)).resized(human.faceIndex <= 10 ? data.faceSizeS : data.faceSizeL)
-				.drawAt(human.pos + Vec2(0, 10) * Math::Sin(human.waveOffset + Time::GetMillisec() / 1000.0), ColorF{ 1.0,1.0,1.0, 1.0 - human.drowningTime / data.drownTime });
+				.drawAt(pos, ColorF{ 1.0,1.0,1.0,human.isSaved ? 1.0 : 1.0 - human.drowningTime / data.drownTime });
+
+			//浮き輪手前
+			if (human.isSaved)
+			{
+				Transformer2D t(Mat3x2::Scale(1, 0.6, pos));
+				for (auto i : step(4)) {
+					Circle{ pos,data.swimRingSize }.drawArc(+90_deg + 45_deg * i, 45_deg, data.swimRingSize / 3, 0, i % 2 == 0 ? Palette::White : Palette::Red);
+				}
+			}
 		}
 	}
 };
